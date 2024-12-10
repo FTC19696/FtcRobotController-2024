daf567a9527d08e7fdbcd2835cfbd79d78bafb35
Scott Hilleque <scott@hilleque.net>
2024-12-10 16:35:03 -0600

    Comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechnicalStuff5.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechnicalStuff5.java
@@ -26,7 +26,9 @@ public class TechnicalStuff5 {
         claw = new Claw(hardwareMap);
     }
 
-    // region Nested Classes
+    // Nested Classes
+
+    // region Chassis Class
 
     public static class Chassis {
         private static final double TICKS_PER_CM = 12.8;
@@ -159,12 +161,16 @@ public class TechnicalStuff5 {
         }
     }
 
+    //endregion
+
+    // region Lift Class
+
     public class Lift {
         public DcMotor motors;
 
         public Lift(HardwareMap hardwareMap) {
             if (hardwareMap == null)
-                throw new IllegalArgumentException("Attempted to construct TechnicalStuff5 object using" +
+                throw new IllegalArgumentException("Attempted to construct Lift object using" +
                         "an uninitialized HardwareMap. Make sure to call this constructor only from" +
                         "an OpMode after initialization (ex. from inside the runOpMode() method).");
 
@@ -182,13 +188,17 @@ public class TechnicalStuff5 {
         }
     }
 
+    // endregion
+
+    // region Elbow Class
+
     public static class Elbow {
         public DcMotor leftMotor;
         public DcMotor rightMotor;
 
         public Elbow(HardwareMap hardwareMap) {
             if (hardwareMap == null)
-                throw new IllegalArgumentException("Attempted to construct TechnicalStuff5 object using" +
+                throw new IllegalArgumentException("Attempted to construct Elbow object using" +
                         "an uninitialized HardwareMap. Make sure to call this constructor only from" +
                         "an OpMode after initialization (ex. from inside the runOpMode() method).");
 
@@ -207,12 +217,16 @@ public class TechnicalStuff5 {
         }
     }
 
+    //endregion
+
+    // region Arm Class
+
     public static class Arm {
         public CRServo extensionServo;
 
         public Arm(HardwareMap hardwareMap) {
             if (hardwareMap == null)
-                throw new IllegalArgumentException("Attempted to construct TechnicalStuff5 object using" +
+                throw new IllegalArgumentException("Attempted to construct Arm object using" +
                         "an uninitialized HardwareMap. Make sure to call this constructor only from" +
                         "an OpMode after initialization (ex. from inside the runOpMode() method).");
 
@@ -220,13 +234,17 @@ public class TechnicalStuff5 {
         }
     }
 
+    // endregion
+
+    // region Claw Class
+
     public static class Claw {
         public Servo leftServo;
         public Servo rightServo;
 
         public Claw(HardwareMap hardwareMap) {
             if (hardwareMap == null)
-                throw new IllegalArgumentException("Attempted to construct TechnicalStuff5 object using" +
+                throw new IllegalArgumentException("Attempted to construct Claw object using" +
                         "an uninitialized HardwareMap. Make sure to call this constructor only from" +
                         "an OpMode after initialization (ex. from inside the runOpMode() method).");
 
